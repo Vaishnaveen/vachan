130043b7c53586fa3385231c5f013e3fb3cfdce7
Vachanamruth <49383698+Vachanamruth@users.noreply.github.com>
2019-04-19 16:50:20 +0530

    Update add_n_arrays.c

--- add_n_arrays.c
+++ add_n_arrays.c
@@ -4,38 +4,45 @@ void inputn(int *n)
      scanf("%d",n);
      return;
 }
-void inputnumbers(int *n, int a[])
+void inputnumbers(int n, int a[])
 {
-      int p=*n;
-      for(int i=0;i<p;i++)
+
+      for(int i=0;i<n;i++)
       {
           scanf("%d",&a[i]);
       }
       return;
 }
-void compute(int *n, int a[],int *sum)
+void compute(int n, int a[],int *sum)
 {
-      int p=*n;
-      for(int i=0;i<p;i++)
+
+      for(int i=0;i<n;i++)
       {
           *sum+=a[i];
       }
       return;
 }
-void output(int sum)
+void output(int a[],int n,int sum)
 {
-      printf("the sum is %d",sum);
+      printf(" ");
+      for(int i=0;i<n-1;i++)
+      {
+         printf("%d + ",a[i]);
+      }
+      printf("%d = %d",a[n],sum);
+
       return;
 }
 int main()
 {
       int n,a[50],sum;
-      printf("read n\n");
+i     printf("read n\n");
       inputn(&n);
       printf("read the numbers\n");
-      inputnumbers(&n,a);
-      compute(&n,a,&sum);
-      output(sum);
+      inputnumbers(n,a);
+      compute(n,a,&sum);
+      output(a,n,sum);
       return 0;
 }
+
 
