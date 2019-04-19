36035858de4a03696717438dcb9ada677a1c6a14
Vachanamruth <49383698+Vachanamruth@users.noreply.github.com>
2019-04-19 15:08:01 +0530

    Create add_n_arrays.c

---
+++ add_n_arrays.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void inputn(int *n)
+{
+     scanf("%d",n);
+     return;
+}
+void intputnumbers(int n, int *a[0])
+{
+      for(int i=0;i<n;i++)
+      {
+          scanf("%d",a[i]);
+      }
+      return;
+}
+void compute(int n, int *a[0],int *sum)
+{     
+      for(int i=0;i<n;i++)
+      {
+          *sum+=*a[i];
+      }
+      return;
+}
+void output(int sum)
+{
+      printf("the sum is %d",sum);
+      return;
+}      
+int main()
+{
+      int i,n,a[50],sum;
+      printf("read n\n");
+      inputn(&n);
+      printf("read the numbers\n");
+      inputnumbers(n,&a[0]);
+      compute(n,&a[],&sum);
+      output(sum);
+      return 0;
+}
+          
