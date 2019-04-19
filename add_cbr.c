a8df4413bba50093a7ffadcf1c1452533ce2876f
Vachanamruth <49383698+Vachanamruth@users.noreply.github.com>
2019-04-19 12:09:47 +0530

    Update add_cbr.c

--- add_cbr.c
+++ add_cbr.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-void input(int &a)
-{     scanf("%d,*a);
+void input(int *a)
+{
+      scanf("%d",a);
       return;
 }
-void compute(int &a)
+void compute(int *a)
 {     int m;
       m=*a+1;
+      *a=m;
       return;
 }
-void output(int &a, int &temp)
-{     printf("the input number is %d\n and the updated value is %d\n",*a,*temp);
+void output(int a, int temp)
+{     printf("the input number is %d\nand the updated value is %d\n",temp,a);
       return;
-}      
-void main()
-{     
+}
+int main()
+{
       int a,temp;
-      printf("read a");
-      input(*a);
+      printf("read a\n");
+      input(&a);
       temp=a;
-      compute(*a);
-      output(*a,*temp);
-      return;
-}      
+      compute(&a);
+      output(a,temp);
+      return 0;
+
+}
